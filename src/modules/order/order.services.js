dc4ef4b01757452ac0adaf4a7159cc132d2cfb88
Adjaro Ogaga <ogagaadjaro@gmail.com>
2021-08-30 07:56:27 +0100

    fix- Fixed port issues

--- src/modules/order/order.services.js
+++ src/modules/order/order.services.js
@@ -1,29 +1,50 @@
 const mongoose = require("mongoose");
 const axios = require('axios').default;
+const Product = require("../models/product.model");
 
-exports.makeAnOrder = async (res, data) => {
+exports.makeAnOrder = async (data) => {
   try {
-   
+    //  check if product exist
+    console.log(data)
+   const product = await Product.findOne({product_name: data.data.product_name});
+   console.log("product: ", product)
+   if(!product){
+     return {
+       error: true,
+       message: "Product Does Not Exist",
+       data:null
+     }
+   }
+   if(product.quantity_in_stock < data.data.quantity){
+     return {
+       error: true,
+       message: "Qunatity of product is not available",
+       data: null
+     }
+   }
+   console.log("PAYMENT_URI: ", process.env.PAYMENT_SERVICE_URI)
+   const Data = data.data;
     const order = await axios.post(
       process.env.PAYMENT_SERVICE_URI,
-      { data },
+      Data,
       {
         headers: {
           'Content-Type': 'application/json',
+          // "Access-Control-Allow-Origin": "*"
       }
       }
     );
-    console.log(order);
+    console.log("ORDER:", order);
     return {
       error: false,
       message: "Order placed successfully",
       data: null,
     };
   } catch (err) {
-    console.log(err?.response?.data || err);
+    console.log(err);
     return {
       error: true,
-      message: "Error making payment",
+      message: "Error placing your order, try again",
       data: err?.response?.data || err,
     };
   }
